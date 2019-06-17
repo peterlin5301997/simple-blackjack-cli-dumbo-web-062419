f635dff4743aaf83ca7f69ee06f92c0ae78dd9da
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-17 19:21:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,9 +32,9 @@ end
 
 def initial_round
   # code #initial_round here
-  sum = deal_card + deal_card
-  display_card_total(sum)
-  return sum
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  return card_total
 end
 
 def hit?
